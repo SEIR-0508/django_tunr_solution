cc5ba8b621a4284da905a6f5cabbc5ae192a6b7f
Jeremy Taubman <56704648+taubman33@users.noreply.github.com>
2023-03-29 15:29:17 -0400

    Update forms.py

--- tunr_django_solution/tunr/forms.py
+++ tunr_django_solution/tunr/forms.py
@@ -1,15 +1,1 @@
 from django import forms
-from .models import Artist, Song
-
-class ArtistForm(forms.ModelForm):
-
-    class Meta:
-        model = Artist
-        fields = ('name', 'photo_url', 'nationality')
-
-
-class SongForm(forms.ModelForm):
-
-    class Meta:
-        model = Song
-        fields = ('artist', 'title', 'album', 'preview_url')
